2b4742b76a4492f04cdc23d0bf6bb3c4fb079694
Karamjit Singh Bedi <74979722+why-kj-why@users.noreply.github.com>
2024-09-13 22:20:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,7 +128,7 @@ def store_manager_app():
         cur = conn.cursor()
         cur.execute(sql_query)
         getDataTable = cur.fetchall()
-        columns = [column[0] for column in cursor.description]
+        columns = [column[0] for column in cur.description]
         getDataTable = pd.DataFrame(getResult, columns=columns)
 
         st.dataframe(getDataTable)
